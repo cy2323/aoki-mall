952b106bc4a7986ffcbc2ec46d8d9968c600d2a7
CaoYong <cy2323@yeah.net>
2020-10-01 23:25:28 +0800

    第一次提交代码

---
+++ aoki-mall-sys/src/main/java/com/aoki/mall/sys/AokiMallSysApplication.java
@@ -0,0 +1,13 @@
+package com.aoki.mall.sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AokiMallSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AokiMallSysApplication.class, args);
+    }
+
+}
---
+++ aoki-mall-sys/src/test/java/com/aoki/mall/sys/AokiMallSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aoki.mall.sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AokiMallSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
