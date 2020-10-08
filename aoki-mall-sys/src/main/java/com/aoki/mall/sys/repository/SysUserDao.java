4723730753e693f3e493e7fd2bf5f5f4a69a76e8
CaoYong <cy2323@yeah.net>
2020-10-08 17:41:57 +0800

    完成登录业务

--- aoki-mall-sys/src/main/java/com/aoki/mall/sys/repository/SysUserDao.java
+++ aoki-mall-sys/src/main/java/com/aoki/mall/sys/repository/SysUserDao.java
@@ -1,11 +1,3 @@
-/**
- * Copyright (c) 2016-2019 人人开源 All rights reserved.
- *
- * https://www.renren.io
- *
- * 版权所有，侵权必究！
- */
-
 package com.aoki.mall.sys.repository;
 
 import com.aoki.mall.sys.entity.SysUserEntity;
@@ -17,7 +9,7 @@ import java.util.List;
 /**
  * 系统用户
  *
- * @author Mark sunlightcs@gmail.com
+ * @author Leo
  */
 @Mapper
 public interface SysUserDao extends BaseMapper<SysUserEntity> {
